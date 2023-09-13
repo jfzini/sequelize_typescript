60f84bc0ad87fa71d6848a69aa40aabf04fac218
João Felipe Zini <joaofelipezini@gmail.com>
2023-09-13 15:53:15 -0300

    req 01 tests

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,20 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import ProductModel from '../../../src/database/models/product.model';
+import app from '../../../src/app';
+import { productMock, resCreatedProduct, sentProduct } from '../../mocks/products.mocks';
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
+
+  it('should return 201 when product is created', async function () {
+    sinon.stub(ProductModel, 'create').resolves(ProductModel.build(productMock));
+    const res = await chai.request(app).post('/products').send(sentProduct);
+    expect(res).to.have.status(201);
+    expect(res.body).to.be.deep.equal(resCreatedProduct);
+  });
 
 });
---
+++ tests/mocks/products.mocks.ts
@@ -0,0 +1,5 @@
+export const productMock = { id: 1, name: 'Product 1', price: 100, orderId: 1 };
+
+export const sentProduct = { name: 'Product 1', price: 100, orderId: 1 };
+
+export const resCreatedProduct = { id: 1, name: 'Product 1', price: 100 };
