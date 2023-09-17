e60ec25407664d0f954b0d0a3ff69f497fe73b02
João Felipe Zini <joaofelipezini@gmail.com>
2023-09-17 19:15:23 -0300

    test: orders middlewares unit tests

---
+++ tests/unit/middlewares/orders.middlewares.test.ts
@@ -0,0 +1,85 @@
+import chai, { expect } from 'chai';
+import { Request, Response } from 'express';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import middlewares from '../../../src/middlewares/orders.middlewares';
+
+chai.use(sinonChai);
+
+describe('Orders Middlewares', function () {
+  const req = {} as Request;
+  const res = {} as Response;
+  const next = sinon.stub();
+
+  beforeEach(function () {
+    sinon.restore();
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  context('validateUserId works as expected', function () {
+    it("should return status BAD REQUEST if userId isn't sent", async function () {
+      req.body = {};
+      await middlewares.validadeUserId(req, res, next);
+
+      expect(res.status).to.have.been.calledWith(400);
+      expect(res.json).to.have.been.calledWith({ message: '"userId" is required' });
+    });
+
+    it("should return status UNPROCESSABLE ENTITY if userId isn't a number", async function () {
+      req.body = { userId: '1' };
+      await middlewares.validadeUserId(req, res, next);
+
+      expect(res.status).to.have.been.calledWith(422);
+      expect(res.json).to.have.been.calledWith({ message: '"userId" must be a number' });
+    });
+
+    it('should call next if userId is a number', async function () {
+      req.body = { userId: 1 };
+      await middlewares.validadeUserId(req, res, next);
+
+      expect(next).to.have.been.called;
+    });
+  });
+
+  context('validateProductIds works as expected', function () {
+    it("should return status BAD REQUEST if productIds isn't sent", async function () {
+      req.body = {};
+      await middlewares.validateProductIds(req, res, next);
+
+      expect(res.status).to.have.been.calledWith(400);
+      expect(res.json).to.have.been.calledWith({ message: '"productIds" is required' });
+    });
+
+    it("should return status UNPROCESSABLE ENTITY if productIds isn't an array", async function () {
+      req.body = { productIds: '1' };
+      await middlewares.validateProductIds(req, res, next);
+
+      expect(res.status).to.have.been.calledWith(422);
+      expect(res.json).to.have.been.calledWith({ message: '"productIds" must be an array' });
+    });
+
+    it("should return status UNPROCESSABLE ENTITY if productIds isn't an array of numbers", async function () {
+      req.body = { productIds: [1, '2'] };
+      await middlewares.validateProductIds(req, res, next);
+
+      expect(res.status).to.have.been.calledWith(422);
+      expect(res.json).to.have.been.calledWith({ message: '"productIds" must include only numbers' });
+    });
+
+    it("should return status UNPROCESSABLE ENTITY if productIds an empty array", async function () {
+      req.body = { productIds: [] };
+      await middlewares.validateProductIds(req, res, next);
+
+      expect(res.status).to.have.been.calledWith(422);
+      expect(res.json).to.have.been.calledWith({ message: '"productIds" must include only numbers' });
+    });
+
+    it('should call next if productIds is an array of numbers', async function () {
+      req.body = { productIds: [1, 2] };
+      await middlewares.validateProductIds(req, res, next);
+
+      expect(next).to.have.been.called;
+    });
+  });
+});
